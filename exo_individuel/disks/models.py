900bb1adb7fa81e4fc6f2952cb307b540673e17f
Célia Cornec <celia.cornec@student.isae-supaero.fr>
2021-05-12 23:22:45 +0200

    Création des modèles Track, Album et Artist

--- exo_individuel/disks/models.py
+++ exo_individuel/disks/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField
+    bytes = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=4, decimal_places=2)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "track"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "album"
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "artist"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
